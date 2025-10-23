8d8ccd81134a67ca42d83e591b4fd404ee534862
SerjaoDev <varollosamuel@gmail.com>
2025-10-23 14:56:59 -0300

    Projeto no Visual Studio

---
+++ Relatorio/Relatorio/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Relatorio
+{
+    partial class FrmMapa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMapa));
+            this.SuspendLayout();
+            // 
+            // FrmMapa
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(900, 600);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmMapa";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Lime;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
